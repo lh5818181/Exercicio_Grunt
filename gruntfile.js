13f9ce576338fb68a4ae254c4ee83fe610704201
Luis Henrique <luish7572@gmail.com>
2024-07-29 17:10:29 -0300

    commit inicial

--- gruntfile.js
+++ gruntfile.js
@@ -57,6 +57,10 @@ module.exports = function(grunt) {
                         {
                             match: 'ENDERECO_DO_CSS',
                             replacement: './styles/main.min.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: './scripts/main.min.js'
                         }
                     ]
                 },
@@ -82,7 +86,13 @@ module.exports = function(grunt) {
             }
         },
         clean: ['prebuild'],
-
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
+                }
+            }
+        }
     })
 
 
@@ -91,7 +101,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('default', ['watch'])
-    grunt.registerTask('build', ['less:production','htmlmin:dist','replace:dist','clean'])
+    grunt.registerTask('build', ['less:production','htmlmin:dist','replace:dist','clean','uglify'])
 }
